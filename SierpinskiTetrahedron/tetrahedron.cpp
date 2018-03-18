6e1ccfae664eea9bdce6eb49fd247feb02692c41
milosz <miloszloszek@gmail.com>
2018-03-18 14:12:55 +0100

    sierpinski tetrahedron

---
+++ SierpinskiTetrahedron/tetrahedron.cpp
@@ -0,0 +1,264 @@
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <stdlib.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+enum
+{
+	FULL_WINDOW,
+	ASPECT_1_1,
+	ABOUT,
+	EXIT
+};
+
+int aspect = FULL_WINDOW;
+
+GLfloat scale = 1.0;
+
+GLfloat rotatex = 0.0;
+GLfloat rotatey = 0.0;
+GLfloat rotatez = 0.0;
+
+GLfloat translatex = 0.0;
+GLfloat translatey = 0.0;
+
+GLuint depth = 1;
+
+void drawTetrahedron(float vertex1[], float vertex2[], float vertex3[], float vertex4[], int currentDepth)
+{
+	if (currentDepth >= depth)
+	{
+		glColor3f(1.0, 0.0, 0.0);
+		glBegin(GL_TRIANGLES);
+		glVertex3f(vertex1[0], vertex1[1], vertex1[2]);
+		glVertex3f(vertex2[0], vertex2[1], vertex2[2]);
+		glVertex3f(vertex3[0], vertex3[1], vertex3[2]);
+		glEnd();
+
+		glColor3f(1.0, 0.0, 1.0);
+		glBegin(GL_TRIANGLES);
+		glVertex3f(vertex1[0], vertex1[1], vertex1[2]);
+		glVertex3f(vertex3[0], vertex3[1], vertex3[2]);
+		glVertex3f(vertex4[0], vertex4[1], vertex4[2]);
+		glEnd();
+
+		glColor3f(0.0, 1.0, 1.0);
+		glBegin(GL_TRIANGLES);
+		glVertex3f(vertex2[0], vertex2[1], vertex2[2]);
+		glVertex3f(vertex3[0], vertex3[1], vertex3[2]);
+		glVertex3f(vertex4[0], vertex4[1], vertex4[2]);
+		glEnd();
+
+		glColor3f(0.0, 0.0, 1.0);
+		glBegin(GL_TRIANGLES);
+		glVertex3f(vertex2[0], vertex2[1], vertex2[2]);
+		glVertex3f(vertex1[0], vertex1[1], vertex1[2]);
+		glVertex3f(vertex4[0], vertex4[1], vertex4[2]);
+		glEnd();
+	}
+	else
+	{
+		float newVertex1[3] = { vertex1[0], vertex1[1], vertex1[2] };
+		float newVertex2[3] = { (vertex1[0] + vertex2[0]) / 2, (vertex1[1] + vertex2[1]) / 2 , (vertex1[2] + vertex2[2]) / 2 };
+		float newVertex3[3] = { vertex2[0], vertex2[1], vertex2[2] };
+		float newVertex4[3] = { (vertex2[0] + vertex3[0]) / 2, (vertex2[1] + vertex3[1]) / 2 , (vertex2[2] + vertex3[2]) / 2 };
+		float newVertex5[3] = { vertex3[0], vertex3[1], vertex3[2] };
+		float newVertex6[3] = { (vertex3[0] + vertex1[0]) / 2, (vertex3[1] + vertex1[1]) / 2 , (vertex3[2] + vertex1[2]) / 2 };
+		float newVertex7[3] = { (vertex1[0] + vertex4[0]) / 2, (vertex1[1] + vertex4[1]) / 2 , (vertex1[2] + vertex4[2]) / 2 };
+		float newVertex8[3] = { (vertex2[0] + vertex4[0]) / 2, (vertex2[1] + vertex4[1]) / 2 , (vertex2[2] + vertex4[2]) / 2 };
+		float newVertex9[3] = { (vertex3[0] + vertex4[0]) / 2, (vertex3[1] + vertex4[1]) / 2 , (vertex3[2] + vertex4[2]) / 2 };
+		float newVertex10[3] = { vertex4[0], vertex4[1], vertex4[2] };
+
+		drawTetrahedron(newVertex1, newVertex2, newVertex6, newVertex7, currentDepth + 1);
+		drawTetrahedron(newVertex2, newVertex3, newVertex4, newVertex8, currentDepth + 1);
+		drawTetrahedron(newVertex6, newVertex4, newVertex5, newVertex9, currentDepth + 1);
+		drawTetrahedron(newVertex7, newVertex8, newVertex9, newVertex10, currentDepth + 1);
+	}
+}
+
+void Display()
+{
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(0.0, 0.0, -3);
+	glScalef(scale, scale, scale);
+	glRotatef(rotatex, 1, 0, 0);
+	glRotatef(rotatey, 0, 1, 0);
+
+	float vertex1[3] = { 0.0, -0.433, 0.433 };
+	float vertex2[3] = { 0.5, -0.433, -0.433 };
+	float vertex3[3] = { -0.5, -0.433, -0.433 };
+	float vertex4[3] = { 0.0, 0.433, 0 };
+
+	drawTetrahedron(vertex1, vertex2, vertex3, vertex4, 1);
+	
+
+	for (unsigned int i = 1; i < 3; i++)
+	{
+		glPushMatrix();
+		glTranslatef((0.5 * i), 0, (-0.866 * i));
+		drawTetrahedron(vertex1, vertex2, vertex3, vertex4, 1);	
+		glPopMatrix();
+		glPushMatrix();
+		glTranslatef((-0.5 * i), 0, (-0.866 * i));
+		drawTetrahedron(vertex1, vertex2, vertex3, vertex4, 1);
+		glPopMatrix();
+	}
+
+	/*float vertices[] =
+	{
+		0.0, 0.0, 0.0,
+		0.5, 0.0, 0.866,
+		-0.5, 0.0, 0.866,
+
+		0.0, 0.0, 0.0,
+		0.5, 0.0, 0.866,
+		0.0, 0.866, 0.433,
+	};*/
+
+
+	/*GLuint myBuffer;
+	glGenBuffers(1, &myBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, myBuffer);
+	glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(float), vertices, GL_STATIC_DRAW);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);*/
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+void Reshape(int width, int height)
+{
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	if (aspect == ASPECT_1_1)
+	{
+		if (width < height && width > 0)
+		{
+			glFrustum(-2.0, 2.0, -2.0 * height / width, 2.0 * height / width, 1, 100);
+		}
+		else if (width >= height && height > 0)
+		{
+			glFrustum(-2.0 * width / height, 2.0 * width / height, -2.0, 2.0, 1.0, 100);
+		}
+	}
+	else
+	{
+		glFrustum(-2.0, 2.0, -2.0, 2.0, 1, 100);
+	}
+
+	Display();
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	if (key == '+')
+	{
+		scale += 0.1;
+	}
+	else if (key == '-' && scale > 0.1)
+	{
+		scale -= 0.1;
+	}
+	else if (key == 'm')
+	{
+		depth += 1;
+	}
+	else if (key == 'n')
+	{
+		depth -= 1;
+	}
+
+	Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+}
+
+void SpecialKeys(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_LEFT:
+		rotatey -= 1;
+		break;
+	case GLUT_KEY_UP:
+		rotatex -= 1;
+		break;
+	case GLUT_KEY_RIGHT:
+		rotatey += 1;
+		break;
+	case GLUT_KEY_DOWN:
+		rotatex += 1;
+		break;
+	}
+
+	Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+}
+
+void About()
+{
+	MessageBox(nullptr, TEXT("I am about to take a shit"), TEXT("About"), MB_OK);
+}
+
+void Menu(int value)
+{
+	switch (value)
+	{
+	case FULL_WINDOW:
+		aspect = FULL_WINDOW;
+		Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+		break;
+	case ASPECT_1_1:
+		aspect = ASPECT_1_1;
+		Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+		break;
+	case ABOUT:
+		About();
+		break;
+	case EXIT:
+		exit(0);
+	}
+}
+
+int main(int argc, char * argv[])
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+	glutInitWindowSize(600, 600);
+
+	glutCreateWindow("Tetrahedron 666");
+
+	glutDisplayFunc(Display);
+
+	glutReshapeFunc(Reshape);
+
+	glutKeyboardFunc(Keyboard);
+	glutSpecialFunc(SpecialKeys);
+
+	int MenuAspect = glutCreateMenu(Menu);
+	glutAddMenuEntry("Aspekt obrazu - cale okno", FULL_WINDOW);
+	glutAddMenuEntry("Aspekt obrazu 1:1", ASPECT_1_1);
+
+	glutCreateMenu(Menu);
+	glutAddSubMenu("Aspekt obrazu", MenuAspect);
+	glutAddMenuEntry("Exit", EXIT);
+
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+	glutMainLoop();
+	return 0;
+}
--- StosModelowania/stosModelowania.cpp
+++ StosModelowania/stosModelowania.cpp
@@ -2,20 +2,14 @@
 #include <GL\freeglut.h>
 #include <stdlib.h>
 
-// sta³e do obs³ugi menu podrêcznego
-
 enum
 {
-	FULL_WINDOW, // aspekt obrazu - ca³e okno
-	ASPECT_1_1, // aspekt obrazu 1:1
-	EXIT // wyjœcie
+	FULL_WINDOW,
+	ASPECT_1_1,
+	EXIT
 };
 
-// aspekt obrazu
-
 int aspect = FULL_WINDOW;
-
-// rozmiary bry³y obcinania
 
 const GLdouble left = -10.0;
 const GLdouble right = 10.0;
@@ -24,29 +18,16 @@ const GLdouble top = 10.0;
 const GLdouble myNear = 50.0;
 const GLdouble myFar = 70.0;
 
-// wspó³czynnik skalowania
-
 GLfloat scale = 1.0;
-
-// k¹ty obrotu
 
 GLfloat rotatex = 0.0;
 GLfloat rotatey = 0.0;
 
-// przesuniêcie
-
 GLfloat translatex = 0.0;
 GLfloat translatey = 0.0;
 
-// wskaŸnik naciœniêcia lewego przycisku myszki
-
 int button_state = GLUT_UP;
-
-// po³o¿enie kursora myszki
-
 int button_x, button_y;
-
-// funkcja rysuj¹ca blok 3x3
 
 void Cube3x3()
 {
@@ -69,8 +50,6 @@ void Cube3x3()
 	glutWireCube(1.0);
 }
 
-// funkcja rysuj¹ca blok 2x2
-
 void Cube2x2()
 {
 	glutWireCube(1.0);
@@ -81,8 +60,6 @@ void Cube2x2()
 	glTranslatef(-1.0, 0.0, 0.0);
 	glutWireCube(1.0);
 }
-
-// funkcja rysuj¹ca piramidê z szeœcianów
 
 void Pyramid()
 {
@@ -166,139 +143,90 @@ void Pyramid()
 	glPopMatrix();
 }
 
-// funkcja generuj¹ca scenê 3D
-
 void Display()
 {
-	// kolor t³a - zawartoœæ bufora koloru
 	glClearColor(1.0, 1.0, 1.0, 1.0);
-
-	// czyszczenie bufora koloru
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	// wybór macierzy modelowania
 	glMatrixMode(GL_MODELVIEW);
-
-	// macierz modelowania = macierz jednostkowa
 	glLoadIdentity();
 
-	// przesuniêcie uk³adu wspó³rzêdnych obiektu do œrodka bry³y odcinania
 	glTranslatef(0, 0, -(myNear + myFar) / 2);
-
-	// przesuniêcie obiektu - ruch myszk¹
 	glTranslatef(translatex, translatey, 0.0);
-
-	// skalowanie obiektu - klawisze "+" i "-"
 	glScalef(scale, scale, scale);
-
-	// obroty obiektu - klawisze kursora
 	glRotatef(rotatex, 1.0, 0, 0);
 	glRotatef(rotatey, 0, 1.0, 0);
 
-	// kolor krawêdzi obiektu
 	glColor3f(0.0, 0.0, 0.0);
 
-	// rysowanie piramidy
 	Pyramid();
 
-	// skierowanie poleceñ do wykonania
 	glFlush();
-
-	// zamiana buforów koloru
 	glutSwapBuffers();
 }
 
-// zmiana wielkoœci okna
-
 void Reshape(int width, int height)
 {
-	// obszar renderingu - ca³e okno
 	glViewport(0, 0, width, height);
-
-	// wybór macierzy rzutowania
 	glMatrixMode(GL_PROJECTION);
-
-	// macierz rzutowania = macierz jednostkowa
 	glLoadIdentity();
 
-	// parametry bry³y obcinania
 	if (aspect == ASPECT_1_1)
 	{
-		// wysokoœæ okna wiêksza od wysokoœci okna
 		if (width < height && width > 0)
+		{
 			glFrustum(left, right, bottom * height / width, top * height / width, myNear, myFar);
-		else
-
-			// szerokoœæ okna wiêksza lub równa wysokoœci okna
-			if (width >= height && height > 0)
-				glFrustum(left * width / height, right * width / height, bottom, top, myNear, myFar);
-
+		}
+		else if (width >= height && height > 0)
+		{
+			glFrustum(left * width / height, right * width / height, bottom, top, myNear, myFar);
+		}
 	}
 	else
 		glFrustum(left, right, bottom, top, myNear, myFar);
 
-	// generowanie sceny 3D
 	Display();
 }
 
-// obs³uga klawiatury
-
 void Keyboard(unsigned char key, int x, int y)
 {
-	// klawisz +
 	if (key == '+')
 		scale += 0.1;
 	else
-
-		// klawisz -
+	{
 		if (key == '-' && scale > 0.1)
 			scale -= 0.1;
+	}
 
-	// odrysowanie okna
 	Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
 }
-
-// obs³uga klawiszy funkcyjnych i klawiszy kursora
 
 void SpecialKeys(int key, int x, int y)
 {
 	switch (key)
 	{
-		// kursor w lewo
 	case GLUT_KEY_LEFT:
 		rotatey -= 1;
 		break;
-
-		// kursor w górê
 	case GLUT_KEY_UP:
 		rotatex -= 1;
 		break;
-
-		// kursor w prawo
 	case GLUT_KEY_RIGHT:
 		rotatey += 1;
 		break;
-
-		// kursor w dó³
 	case GLUT_KEY_DOWN:
 		rotatex += 1;
 		break;
 	}
 
-	// odrysowanie okna
 	Reshape(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
 }
-
-// obs³uga przycisków myszki
 
 void MouseButton(int button, int state, int x, int y)
 {
 	if (button == GLUT_LEFT_BUTTON)
 	{
-		// zapamiêtanie stanu lewego przycisku myszki
 		button_state = state;
 
-		// zapamiêtanie po³o¿enia kursora myszki
 		if (state == GLUT_DOWN)
 		{
 			button_x = x;
@@ -306,8 +234,6 @@ void MouseButton(int button, int state, int x, int y)
 		}
 	}
 }
-
-// obs³uga ruchu kursora myszki
 
 void MouseMotion(int x, int y)
 {
@@ -375,63 +301,35 @@ void Menu(int value)
 
 int main(int argc, char * argv[])
 {
-	// inicjalizacja biblioteki GLUT
 	glutInit(&argc, argv);
 
-	// inicjalizacja bufora ramki
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
-	// rozmiary g³ównego okna programu
 	glutInitWindowSize(400, 400);
 
-	// utworzenie g³ównego okna programu
 	glutCreateWindow("Stos modelowania");
 
-	// do³¹czenie funkcji generuj¹cej scenê 3D
 	glutDisplayFunc(Display);
 
-	// do³¹czenie funkcji wywo³ywanej przy zmianie rozmiaru okna
 	glutReshapeFunc(Reshape);
 
-	// do³¹czenie funkcji obs³ugi klawiatury
 	glutKeyboardFunc(Keyboard);
 
-	// do³¹czenie funkcji obs³ugi klawiszy funkcyjnych i klawiszy kursora
 	glutSpecialFunc(SpecialKeys);
 
-	// obs³uga przycisków myszki
 	glutMouseFunc(MouseButton);
-
-	// obs³uga ruchu kursora myszki
 	glutMotionFunc(MouseMotion);
 
-	// utworzenie podmenu - aspekt obrazu
 	int MenuAspect = glutCreateMenu(Menu);
-#ifdef WIN32
-
-	glutAddMenuEntry("Aspekt obrazu - ca³e okno", FULL_WINDOW);
-#else
-
 	glutAddMenuEntry("Aspekt obrazu - cale okno", FULL_WINDOW);
-#endif
-
 	glutAddMenuEntry("Aspekt obrazu 1:1", ASPECT_1_1);
 
-	// menu g³ówne
 	glutCreateMenu(Menu);
 	glutAddSubMenu("Aspekt obrazu", MenuAspect);
-#ifdef WIN32
+	glutAddMenuEntry("Exit", EXIT);
 
-	glutAddMenuEntry("Wyjœcie", EXIT);
-#else
-
-	glutAddMenuEntry("Wyjscie", EXIT);
-#endif
-
-	// okreœlenie przycisku myszki obs³uguj¹cego menu podrêczne
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-	// wprowadzenie programu do obs³ugi pêtli komunikatów
 	glutMainLoop();
 	return 0;
 }
